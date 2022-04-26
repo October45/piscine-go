3b8fb86129383698c0149dbbdd345a1eb4884309
October45 <vikto45.t@gmail.com>
2022-04-26 16:14:50 +0300

    Automated commit

--- isprime.go
+++ isprime.go
@@ -3,13 +3,12 @@ package piscine
 func IsPrime(nb int) bool {
 	if nb < 2 {
 		return false
-	}
-	if nb == 2 || nb == 3 || nb == 5 || nb == 7 || nb == 11 {
+	} else if nb == 2 || nb == 3 {
 		return true
-	} else if nb < 11 {
+	} else if nb%2 == 0 {
 		return false
 	}
-	for i := 2; i < nb/2; i++ {
+	for i := 3; i*i <= nb; i += 2 {
 		if nb%i == 0 {
 			return false
 		}
